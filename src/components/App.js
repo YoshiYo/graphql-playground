6ca7555ab82fb78bd5d21544beb1d3088e65bf1d
Guillaume Forgue <guillaume.forgue@y-nov.com>
2018-03-22 15:52:31 +0100

    feat: add an auth system

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Header from './Header'
+import Login from './Login'
 import { Switch, Route } from 'react-router-dom'
 
 
@@ -11,9 +12,10 @@ class App extends Component {
       <div className="center w85">
         <Header />
         <div className="ph3 pv1 background-gray">
-          <Switch>
-            <Route exact path="/" component={LinkList} />
+          <Switch>      
+            <Route exact path="/login" component={Login} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/" component={LinkList} />
           </Switch>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './components/styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { AUTH_TOKEN } from './constants'
+import { ApolloLink } from 'apollo-client-preset'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
@@ -12,9 +14,22 @@ import { BrowserRouter } from 'react-router-dom'
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 })
 
